9a3f661e5826a18e22863e5cf872965376d7c42e
kazki_kurita <kazki.kurita@gmail.com>
2019-05-01 12:04:16 +0900

    ログイン処理実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
-  		hoge
+  		log_in(user)
+  		redirect_to user
   	else
   		flash.now[:danger] = '無効なメールアドレス、又はパスワードが間違っています。'
   		render 'new'
