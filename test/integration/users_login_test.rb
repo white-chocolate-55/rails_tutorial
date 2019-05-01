9c5aae1e67a11cb2b266c98dc4c83edc0d10932a
kazki_kurita <kazki.kurita@gmail.com>
2019-05-01 12:50:09 +0900

    ログイン実装

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@user = users(:michael)
+	end
   
   test "無効な情報でログインをした場合" do
   	get login_path
@@ -9,5 +13,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   	assert_template 'sessions/new'
   	get root_path
   	assert flash.empty?
+  end
+
+  test "有効な情報でログインした場合" do
+  	get login_path
+  	post login_path, params: { session: { email: @user.email, password: 'password' } }
+  	assert_redirected_to @user
+  	follow_redirect!
+  	assert_template 'users/show'
+  	assert_select "a[href=?]", login_path, count: 0
+  	assert_select "a[href=?]", logout_path
+  	assert_select "a[href=?]", user_path(@user)
   end
 end
