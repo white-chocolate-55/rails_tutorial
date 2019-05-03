909cb89e88e0c0d4ccbe68d13aaf6e80db9c3a1c
kazki_kurita <kazki.kurita@gmail.com>
2019-05-03 17:48:50 +0900

    リスト9.31のテストが通らない

---
+++ test/helpers/sessions_helper_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class SessionsHelperTest < ActionView::TestCase
+
+	def setup
+		@user = users(:michael)
+		remember(@user)
+	end
+
+	test "セッションがnilの場合、current_userが正常なユーザーを返す" do
+		assert_equal @user, current_user
+		assert is_logged_in?
+	end
+
+	test "remember digestが間違っていた場合、current_userがnilを返す" do
+		@user.update_attribute(:remember_digest, User.digest(User.new_token))
+		assert_nil current_user
+	end
+end
