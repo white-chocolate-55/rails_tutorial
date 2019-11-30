fdf6bd3fc02321a5112761af83b42382acb98104
kazki_kurita <kazki.kurita@gmail.com>
2019-11-30 20:39:52 +0900

    9章終わり

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,7 +21,9 @@ class ActiveSupport::TestCase
   class ActionDispatch::IntegrationTest
 
   	def log_in_as(user, password: 'password', remember_me: '1')
-  		post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me } }
+  		post login_path, params: { session: { email: user.email,
+                                            password: password,
+                                            remember_me: remember_me } }
   	end
   	
 	end
